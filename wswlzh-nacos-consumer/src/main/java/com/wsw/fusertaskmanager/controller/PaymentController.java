7d23975ca49e9a284f256ae00ef906b2c8be0dc2
WSW <2544894086@qq.com>
2020-10-21 22:39:11 +0800

    nacos provider update

---
+++ wswlzh-nacos-consumer/src/main/java/com/wsw/fusertaskmanager/controller/PaymentController.java
@@ -0,0 +1,6 @@
+package com.wsw.fusertaskmanager.controller;/**
+ * @Author WangSongWen
+ * @Date  2020/10/21 下午10:03
+ * @Description:
+ */public class PaymentController {
+}
---
+++ wswlzh-nacos-consumer/src/main/java/com/wsw/fusertaskmanager/service/PaymentFeignService.java
@@ -0,0 +1,6 @@
+package com.wsw.fusertaskmanager.service;/**
+ * @Author WangSongWen
+ * @Date  2020/10/21 下午10:01
+ * @Description:
+ */public interface PaymentFeignService {
+}
