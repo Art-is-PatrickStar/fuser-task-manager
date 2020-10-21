03c49556bf4a39c721e10e17bd11dcf8ba2171ff
WSW <2544894086@qq.com>
2020-10-21 22:39:45 +0800

    nacos consumer update

--- wswlzh-nacos-consumer/src/main/java/com/wsw/fusertaskmanager/controller/PaymentController.java
+++ wswlzh-nacos-consumer/src/main/java/com/wsw/fusertaskmanager/controller/PaymentController.java
@@ -1,6 +1,28 @@
-package com.wsw.fusertaskmanager.controller;/**
+package com.wsw.fusertaskmanager.controller;
+
+import com.oracle.tools.packager.Log;
+import com.wsw.fusertaskmanager.entities.CommonResult;
+import com.wsw.fusertaskmanager.entities.Payment;
+import com.wsw.fusertaskmanager.service.PaymentFeignService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @Author WangSongWen
- * @Date  2020/10/21 下午10:03
+ * @Date 2020/10/21 下午10:03
  * @Description:
- */public class PaymentController {
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Autowired
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/consumer/nacos/payment/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
 }
--- wswlzh-nacos-consumer/src/main/java/com/wsw/fusertaskmanager/service/PaymentFeignService.java
+++ wswlzh-nacos-consumer/src/main/java/com/wsw/fusertaskmanager/service/PaymentFeignService.java
@@ -1,6 +1,20 @@
-package com.wsw.fusertaskmanager.service;/**
+package com.wsw.fusertaskmanager.service;
+
+import com.wsw.fusertaskmanager.entities.CommonResult;
+import com.wsw.fusertaskmanager.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
  * @Author WangSongWen
- * @Date  2020/10/21 下午10:01
- * @Description:
- */public interface PaymentFeignService {
+ * @Date 2020/10/21 下午10:01
+ * @Description: 使用openFeign进行服务调用
+ */
+@Service
+@FeignClient(value = "nacos-payment-provider")
+public interface PaymentFeignService {
+    @GetMapping("/nacos/payment/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
 }
