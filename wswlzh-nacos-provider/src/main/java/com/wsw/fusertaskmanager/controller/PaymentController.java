534ebfe2c90b5f06ea0b0f201101ef4a7f715314
WSW <2544894086@qq.com>
2020-10-21 22:39:26 +0800

    nacos provider update

--- wswlzh-nacos-provider/src/main/java/com/wsw/fusertaskmanager/controller/PaymentController.java
+++ wswlzh-nacos-provider/src/main/java/com/wsw/fusertaskmanager/controller/PaymentController.java
@@ -1,5 +1,10 @@
 package com.wsw.fusertaskmanager.controller;
 
+import com.wsw.fusertaskmanager.entities.CommonResult;
+import com.wsw.fusertaskmanager.entities.Payment;
+import com.wsw.fusertaskmanager.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
+@Slf4j
 public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+    @Autowired
+    private PaymentService paymentService;
+
     @GetMapping("/nacos/payment/{id}")
-    public String test(@PathVariable("id") String id){
-        return "nacos registry. id = " + id + ", poet = " + serverPort;
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("查询结果: " + payment);
+
+        if (null != payment){
+            return new CommonResult(200, "查询数据成功, serverPort: " + serverPort, payment);
+        }else {
+            return new CommonResult(444, "查询数据失败, id = " + id, null);
+        }
     }
 }
