7971091798580da9613396897bf3aae7f94ad310
wangsongwen <2544894086wsw@gmail.com>
2020-10-26 14:46:18 +0800

    provider cluster

---
+++ wswlzh-nacos-provider2/src/test/java/com/wsw/fusertaskmanager/WswlzhNacosProvider2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsw.fusertaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WswlzhNacosProvider2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
