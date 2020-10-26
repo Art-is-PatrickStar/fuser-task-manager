b839a44efd3365afe53393ac10b8a5a580a47c3b
wangsongwen <2544894086wsw@gmail.com>
2020-10-26 15:40:36 +0800

    shit can't use it

--- wswlzh-nacos-consumer/src/main/java/com/wsw/fusertaskmanager/service/PaymentService.java
+++ wswlzh-nacos-consumer/src/main/java/com/wsw/fusertaskmanager/service/PaymentService.java
@@ -3,6 +3,7 @@ package com.wsw.fusertaskmanager.service;
 import com.wsw.fusertaskmanager.entities.CommonResult;
 import com.wsw.fusertaskmanager.entities.Payment;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @Date 2020/10/21 下午10:01
  * @Description: 使用openFeign进行服务调用
  */
+@Service
 @FeignClient(value = "nacos-payment-provider", fallback = FallBackPaymentService.class)
 public interface PaymentService {
     @GetMapping("/nacos/payment/{id}")
