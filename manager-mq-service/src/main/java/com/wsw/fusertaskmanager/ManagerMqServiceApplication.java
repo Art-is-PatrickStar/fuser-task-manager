55eab9e42b44d43ebfd2b91b33995b542e27504d
wangsongwen <2544894086wsw@gmail.com>
2020-11-05 11:02:02 +0800

    2020-11-05 update

---
+++ manager-mq-service/src/main/java/com/wsw/fusertaskmanager/ManagerMqServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wsw.managermqservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerMqServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerMqServiceApplication.class, args);
+	}
+
+}
---
+++ manager-mq-service/src/test/java/com/wsw/fusertaskmanager/ManagerMqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsw.managermqservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerMqServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
