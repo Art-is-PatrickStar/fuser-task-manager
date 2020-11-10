de2bd068d317e55a3d6f6235edf2f46d8c71bb58
wangsongwen <2544894086wsw@gmail.com>
2020-11-10 16:10:58 +0800

    前后端分离 跨域处理

---
+++ manager-task-service/src/main/java/com/wsw/fusertaskmanager/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.wsw.fusertaskmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author WangSongWen
+ * @Date: Created in 16:01 2020/11/10
+ * @Description:
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    /**
+     * 跨域设置
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 允许跨域访问的路径
+                .allowedOrigins("*")  // 允许跨域访问的资源
+                .allowedHeaders("*")  // 允许头部设置
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 允许请求方法
+                .allowCredentials(true)  // 是否发送cookie
+                .maxAge(168000);  // 预检时间间隔
+    }
+}
