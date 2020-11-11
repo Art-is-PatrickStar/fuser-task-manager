67f1074f06213a82cd8cc07e22e779f81cc0308f
wangsongwen <2544894086wsw@gmail.com>
2020-11-11 11:27:07 +0800

    gateway模块

---
+++ manager-gateway-service/src/main/java/com/wsw/fusertaskmanager/ManagerGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wsw.fusertaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerGatewayServiceApplication.class, args);
+	}
+
+}
