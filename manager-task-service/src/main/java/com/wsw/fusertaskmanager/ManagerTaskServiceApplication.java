a8a3928f67d27f3b51eee8400be547d5703b67b2
wangsongwen <2544894086wsw@gmail.com>
2020-11-11 11:28:02 +0800

    试试redis缓存

--- manager-task-service/src/main/java/com/wsw/fusertaskmanager/ManagerTaskServiceApplication.java
+++ manager-task-service/src/main/java/com/wsw/fusertaskmanager/ManagerTaskServiceApplication.java
@@ -2,6 +2,7 @@ package com.wsw.fusertaskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableScheduling
+@EnableCaching
 public class ManagerTaskServiceApplication {
 
 	public static void main(String[] args) {
--- manager-task-service/src/main/java/com/wsw/fusertaskmanager/service/impl/TaskServiceImpl.java
+++ manager-task-service/src/main/java/com/wsw/fusertaskmanager/service/impl/TaskServiceImpl.java
@@ -3,6 +3,10 @@ package com.wsw.fusertaskmanager.service.impl;
 import com.wsw.fusertaskmanager.domain.Task;
 import com.wsw.fusertaskmanager.mapper.TaskMapper;
 import com.wsw.fusertaskmanager.service.TaskService;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -14,51 +18,61 @@ import java.util.List;
  * @Description:
  */
 @Service
+@CacheConfig(cacheNames = "task")
 public class TaskServiceImpl implements TaskService {
     @Resource
     private TaskMapper taskMapper;
 
     @Override
+    @CachePut(key = "task.taskId")
     public int createTask(Task task) {
         return taskMapper.createTask(task);
     }
 
     @Override
+    @Cacheable(key = "#task.taskId")
     public int updateTaskById(Task task) {
         return taskMapper.updateTaskById(task);
     }
 
     @Override
+    @Cacheable(key = "#task.taskId")
     public int updateTaskByName(Task task) {
         return taskMapper.updateTaskByName(task);
     }
 
     @Override
+    @Cacheable(key = "#taskId")
     public int updateTaskStatusByTaskId(Long taskId, char taskStatus) {
         return taskMapper.updateTaskStatusByTaskId(taskId, taskStatus);
     }
 
     @Override
+    @CacheEvict(key = "#taskId")
     public int deleteTaskByTaskId(Long taskId) {
         return taskMapper.deleteTaskByTaskId(taskId);
     }
 
     @Override
+    @CacheEvict(key = "#taskName")
     public int deleteTaskByTaskName(String taskName) {
         return taskMapper.deleteTaskByTaskName(taskName);
     }
 
     @Override
+    @Cacheable(key = "#taskId")
     public Task selectTaskById(Long taskId) {
         return taskMapper.selectTaskById(taskId);
     }
 
     @Override
+    @Cacheable(key = "#taskName")
     public List<Task> selectTaskByName(String taskName) {
         return taskMapper.selectTaskByName(taskName);
     }
 
     @Override
+    @Cacheable(key = "#taskStatus")
     public List<Task> selectTaskByStatus(char taskStatus) {
         return taskMapper.selectTaskByStatus(taskStatus);
     }
