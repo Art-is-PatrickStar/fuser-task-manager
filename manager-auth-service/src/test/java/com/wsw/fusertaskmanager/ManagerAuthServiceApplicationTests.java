2dbd3024dbba806c0143c4a1119de3a9e4f6041c
wangsongwen <2544894086wsw@gmail.com>
2020-11-12 17:35:10 +0800

    认证中心搭建

---
+++ manager-auth-service/src/test/java/com/wsw/fusertaskmanager/ManagerAuthServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wsw.fusertaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
