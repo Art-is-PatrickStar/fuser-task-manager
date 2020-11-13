2cf57030879080c243b0530be591a43c772feb47
wangsongwen <2544894086wsw@gmail.com>
2020-11-13 10:23:38 +0800

    认证中心颁发token

--- manager-auth-service/src/main/java/com/wsw/fusertaskmanager/controller/AuthController.java
+++ manager-auth-service/src/main/java/com/wsw/fusertaskmanager/controller/AuthController.java
@@ -1,13 +1,22 @@
 package com.wsw.fusertaskmanager.controller;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wsw.fusertaskmanager.api.CommonResult;
 import com.wsw.fusertaskmanager.domain.User;
 import com.wsw.fusertaskmanager.service.AuthService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.BASE64Encoder;
 
 import javax.annotation.Resource;
+import javax.crypto.SecretKey;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @Author WangSongWen
@@ -19,17 +28,35 @@ public class AuthController {
     @Resource
     private AuthService authService;
 
+    @Value("${app.secret}")
+    private String key;
+
     @PostMapping("/auth")
-    public CommonResult auth(@RequestParam("username") String username, @RequestParam("password") String password){
-        User user = null;
+    public CommonResult<Map> auth(@RequestParam("username") String username, @RequestParam("password") String password){
+        CommonResult<Map> commonResult = null;
         try {
-            user = authService.auth(username, password);
+            Map<String, Object> map = new LinkedHashMap<>();
+            User user = authService.auth(username, password);
+            map.put("user", user);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+            user.setPassword(null);  // jwt的json数据不能包含敏感信息
+            String userString = objectMapper.writeValueAsString(user);
+
+            // 对密匙进行Base64编码
+            String base64 = new BASE64Encoder().encode(key.getBytes());
+            // 生成密匙对象,会根据base64长度自动选择相应的HMAC算法
+            SecretKey secretKey = Keys.hmacShaKeyFor(base64.getBytes());
+            // 利用jwt生成token
+            String token = Jwts.builder().setSubject(userString).signWith(secretKey).compact();
+            map.put("token", token);
+
+            commonResult = CommonResult.success(map);
         } catch (Exception e) {
-            e.printStackTrace();
+            commonResult = CommonResult.failed();
         }
-        if (null != user){
-            return CommonResult.success(user);
-        }
-        return CommonResult.failed();
+
+        return commonResult;
     }
 }
--- manager-auth-service/src/test/java/com/wsw/fusertaskmanager/ManagerAuthServiceApplicationTests.java
+++ manager-auth-service/src/test/java/com/wsw/fusertaskmanager/ManagerAuthServiceApplicationTests.java
@@ -1,14 +1,47 @@
 package com.wsw.fusertaskmanager;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import sun.misc.BASE64Encoder;
+
+import javax.crypto.SecretKey;
 
 @SpringBootTest
 class ManagerAuthServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void createJwt() {
+		// 密匙
+		String key = "1234567890-1234567890-1234567890";  // 32位
+		// 对密匙进行Base64编码
+		String base64 = new BASE64Encoder().encode(key.getBytes());
+		// 生成密匙对象,会根据base64长度自动选择相应的HMAC算法
+		SecretKey secretKey = Keys.hmacShaKeyFor(base64.getBytes());
+		// 利用jwt生成token
+		String token = Jwts.builder().setSubject("{\"wsw\":123}").signWith(secretKey).compact();
+		System.out.println(token);
+	}
 
+	@Test
+	void checkJwt(){
+		// token
+		String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1aWRcIjoyLFwidXNlcm5hbWVcIjpcImx6aFwiLFwidXNlcm5hbWVDSE5cIjpcIumZhuWtkOaBklwifSJ9.fjqGlnYnS8LySI1NA63BHq67VqT2mG2IBHUV4vQqV2k";
+		// 密匙
+		String key = "1234567890-1234567890-1234567890";
+		// 对密匙进行Base64编码
+		String base64 = new BASE64Encoder().encode(key.getBytes());
+		// 生成密匙对象,会根据base64长度自动选择相应的HMAC算法
+		SecretKey secretKey = Keys.hmacShaKeyFor(base64.getBytes());
+		// 验证token
+		JwtParser parse = Jwts.parserBuilder().setSigningKey(secretKey).build();
+		Jws<Claims> claimsJws = parse.parseClaimsJws(token);
+		String subject = claimsJws.getBody().getSubject();
+		System.out.println(subject);
 	}
 
 }
