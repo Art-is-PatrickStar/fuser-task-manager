dcdab1d806902765ae73be0c1f479c8713391643
wangsongwen <2544894086wsw@gmail.com>
2020-11-13 17:25:23 +0800

    gateway模块 update

--- manager-gateway-service/src/main/java/com/wsw/fusertaskmanager/ManagerGatewayServiceApplication.java
+++ manager-gateway-service/src/main/java/com/wsw/fusertaskmanager/ManagerGatewayServiceApplication.java
@@ -1,14 +1,11 @@
 package com.wsw.fusertaskmanager;
 
-//import com.wsw.fusertaskmanager.config.IgnoreUrlsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableConfigurationProperties(IgnoreUrlsConfig.class)
 public class ManagerGatewayServiceApplication {
 
 	public static void main(String[] args) {
