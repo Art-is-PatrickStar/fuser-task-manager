2cd612409f7dcb13e1827c8d754beba04bbc861a
Wangsongwen <2544894086wsw@gmail.com>
2020-11-18 22:31:55 +0800

    restTemplate update

--- manager-task-service/src/main/java/com/wsw/fusertaskmanager/service/TaskPoolTestService.java
+++ manager-task-service/src/main/java/com/wsw/fusertaskmanager/service/TaskPoolTestService.java
@@ -1,16 +1,16 @@
 package com.wsw.fusertaskmanager.service;
 
+import cn.hutool.core.map.MapUtil;
 import com.alibaba.fastjson.JSON;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @Author WangSongWen
@@ -19,25 +19,35 @@ import java.util.Map;
  */
 @Service
 public class TaskPoolTestService {
-    private String getTaskPoolListURL = "";
-    private RestTemplate restTemplate = new RestTemplate();
-    private Map<String, Object> uriVariablesMap = new LinkedHashMap<>();
+    String getTaskPoolListURL = "http://localhost:1998/manager-task-service/task/dosth";
+    RestTemplate restTemplate = new RestTemplate();
 
-    public List<Map<String, Object>> getTaskPoolList(){
-        List<Map<String, Object>> taskPoolList = new ArrayList<>();
+    public Map<String, Object> getTaskPoolData(){
+        Map<String, Object> taskPoolMap = new HashMap<>();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("token", "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1aWRcIjoxLFwidXNlcm5hbWVcIjpcIndzd1wiLFwidXNlcm5hbWVDSE5cIjpcIueOi-advuaWh1wifSJ9.x91ftNfBWLzRvDFlXw4lz4OrKJtX2QSmKa45zvhftdE");
         ResponseEntity<String> response = null;
         try {
-            response = restTemplate.getForEntity(getTaskPoolListURL, String.class, uriVariablesMap);
+            response = restTemplate.getForEntity(getTaskPoolListURL, String.class, headers);
         } catch (RestClientException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
-        if (response.getStatusCodeValue() == HttpStatus.OK.value()){
-            Map<String, Object> responseBodyMap = JSON.parse(response.getBody(), Map.class);
-
+        if ((response != null ? response.getStatusCodeValue() : 0) == HttpStatus.OK.value()){
+            Map<String, Object> responseBodyMap = JSON.parseObject(response != null ? response.getBody() : null, Map.class);
+            Integer code = MapUtil.getInt(responseBodyMap, "code");
+            if (code == 200 && !MapUtil.isEmpty(responseBodyMap)){
+                String data = MapUtil.getStr(responseBodyMap, "data");
+                if (null != data){
+                    taskPoolMap.put("data", data);
+                }
+            }else {
+                String message = MapUtil.getStr(responseBodyMap, "message");
+                taskPoolMap.put("code", code);
+                taskPoolMap.put("message", message);
+            }
         }
 
-
-        return taskPoolList;
+        return taskPoolMap;
     }
 
 }
--- manager-task-service/src/test/java/com/wsw/fusertaskmanager/ManagerTaskServiceApplicationTests.java
+++ manager-task-service/src/test/java/com/wsw/fusertaskmanager/ManagerTaskServiceApplicationTests.java
@@ -1,13 +1,29 @@
 package com.wsw.fusertaskmanager;
 
+import com.wsw.fusertaskmanager.service.TaskPoolTestService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
 
 @SpringBootTest
 class ManagerTaskServiceApplicationTests {
+	@Autowired
+	private TaskPoolTestService taskPoolTestService;
 
 	@Test
 	void contextLoads() {
+		Map<String, Object> taskPoolMap = taskPoolTestService.getTaskPoolData();
+		Integer code = (Integer) taskPoolMap.get("code");
+		String message = (String) taskPoolMap.get("message");
+		String data = (String) taskPoolMap.get("data");
+		if (code == 200){
+			System.out.println(data);
+		}else {
+			System.out.println(message + ", " + data);
+		}
+
 	}
 
 }
